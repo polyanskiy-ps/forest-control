29bfbf0cc9bcc18891606b09b4d7fbb701988a14
GHOST <raumzzz@yandex.ru>
2022-10-20 10:43:47 +0300

    update CorsFilter

--- src/main/java/ru/topaztree/topaztree/config/CorsFilter.java
+++ src/main/java/ru/topaztree/topaztree/config/CorsFilter.java
@@ -1,7 +1,6 @@
 package ru.topaztree.topaztree.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
